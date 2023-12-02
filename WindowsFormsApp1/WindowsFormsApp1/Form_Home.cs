c5f36c3d6c55c83a87cdf082fc1a079710d730db
Walker McKee <89943198+WalkerMcKee0909@users.noreply.github.com>
2023-12-02 00:24:24 -0600

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form_Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form_Home : Form
+    {
+        private bool isStudent, isProvider, isSuper;
+
+        private void Form_Home_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public Form_Home(bool isStudent, bool isProvider, bool isSuper) {
+            InitializeComponent();
+            this.isStudent = isStudent;
+            this.isProvider = isProvider;
+            this.isSuper = isSuper;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
